b5a804d08f6112edc3dc7e1543cb517eaa3cdc95
johannes <johannesbarja@protonmail.com>
2022-08-04 17:53:40 -0500

    Add centering code

--- app/Main.hs
+++ app/Main.hs
@@ -101,14 +101,18 @@ keyMap =
     ]
 
 displayScreen :: MonadEmulator m => (Int, Int) -> m Picture
-displayScreen (width, height) = fmap pictures . sequence $ do
+displayScreen (w, h) = fmap pictures . sequence $ do
     x <- [0..63]
-    let xPos = fromIntegral x * size - (fromIntegral width / 2)
+    let xOffset = (width - size * 64) / 2
+    let xPos = fromIntegral x * size - width / 2
     y <- [0..31]
-    let yPos = fromIntegral (y + 1) * (-size) + (fromIntegral height / 2)
+    let yOffset = (height - size * 32) / 2
+    let yPos = fromIntegral (y + 1) * (-size) + height / 2
 
-    pure $ pixelImage xPos yPos size <$> look (Gfx x y)
-    where size = pixelSize (fromIntegral width) (fromIntegral height)
+    pure $ pixelImage (xPos + xOffset) (yPos - yOffset) size <$> look (Gfx x y)
+    where size = pixelSize width height
+          width  = fromIntegral w
+          height = fromIntegral h
 
 pixelSize :: (Ord a, Fractional a) => a -> a -> a
 pixelSize width height = min (width / 64) (height / 32)
